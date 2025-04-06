e2c44cac269411c8eda6e984174fccbc5d30af1d
Phileas <phileasmah@gmail.com>
2025-04-06 21:41:41 +0100

    small css tweak

--- src/components/home/new-releases/NewRelease.tsx
+++ src/components/home/new-releases/NewRelease.tsx
@@ -1,10 +1,10 @@
 import { Badge } from "@/components/ui/badge";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { CarouselItem } from "@/components/ui/carousel";
 import { SpotifyAlbum } from "@/types/spotify";
@@ -17,18 +17,18 @@ interface NewReleaseProps {
 
 export function NewRelease({ album }: NewReleaseProps) {
   return (
-    <CarouselItem className="md:basis-1/3 lg:basis-1/5">
+    <CarouselItem className="md:basis-1/3 lg:basis-1/5 py-1">
       <Link
         href={`/albums/${encodeURIComponent(album.artists[0].name)}/${encodeURIComponent(album.name)}?id=${album.id}`}
         className="block"
       >
-        <Card className="overflow-hidden pt-0 transition-colors duration-200 hover:bg-accent">
+        <Card className="hover:bg-accent overflow-hidden pt-0 transition-colors duration-200">
           {album.images[0] && (
             <div className="relative aspect-square w-full">
               <Image
                 src={
                   album.images.reduce((prev, curr) =>
-                    prev.width > curr.width ? prev : curr
+                    prev.width > curr.width ? prev : curr,
                   ).url
                 }
                 alt={album.name}
@@ -52,4 +52,4 @@ export function NewRelease({ album }: NewReleaseProps) {
       </Link>
     </CarouselItem>
   );
-} 
+}
