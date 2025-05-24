faf394b9542af8f4f557c739973cbe2127c4f734
Phileas <phileasmah@gmail.com>
2025-05-25 00:36:53 +0100

    Another small fix

--- src/components/followingActivity/following-activity-small.tsx
+++ src/components/followingActivity/following-activity-small.tsx
@@ -19,7 +19,7 @@ export default function FollowingActivitySmall() {
 
   return (
     <div className="flex flex-col gap-3">
-      <div className="flex items-baseline justify-between">
+      <div className="flex items-center justify-between px-4">
         <h2 className="text-2xl font-bold">Activity from people you follow</h2>
         <ViewAllButton href="/following" />
       </div>
